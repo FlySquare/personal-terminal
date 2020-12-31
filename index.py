2d4b16e67a79f51423222f62af97ee169e7fd2fb
Umut Can Arda <umutkonurinso@gmail.com>
2020-12-31 21:09:59 +0300

    update

---
+++ index.py
@@ -0,0 +1,22 @@
+import os
+
+
+def figlet():
+    print(" ")
+    print("db    db .88b  d88. db    db d888888b ")
+    print("88    88 88'YbdP`88 88    88 `~~88~~' ")
+    print("88    88 88  88  88 88    88    88    ")
+    print("88    88 88  88  88 88    88    88    ")
+    print("88b  d88 88  88  88 88b  d88    88    ")
+    print("~Y8888P' YP  YP  YP ~Y8888P'    YP    ")
+    print(" ")
+
+def komutlar(var):
+
+    if var=="xampp":
+       os.system('sudo /opt/lampp/manager-linux-x64.run')
+    
+    
+
+x = input("umutcan@flysquare:~ ")
+komutlar(x)
